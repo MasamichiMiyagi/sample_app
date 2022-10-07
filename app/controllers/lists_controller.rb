c9e194f4be60b09357d42c0a11f30b3473fc8171
MasamichiMiyagi <114850152+MasamichiMiyagi@users.noreply.github.com>
2022-10-07 07:38:55 +0000

    [finish]3-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,15 @@
 class ListsController < ApplicationController
   def new
+    @list = List.new
+  end
+
+  def create
+    # 1.&2. データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # 4. トップ画面へリダイレクト
+    redirect_to '/top'
   end
 
   def index
@@ -9,5 +19,11 @@ class ListsController < ApplicationController
   end
 
   def edit
+  end
+
+  private #←一種の境界線、「ここから下はこのcontrollerの中でしか呼び出せません」という意味があるので、他アクション(create,index,show等)を巻き込まないように一番下に書く。
+  #↓以下ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
