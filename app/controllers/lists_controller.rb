1bb138e4a200f19574ececf686c170dd370127f0
MasamichiMiyagi <114850152+MasamichiMiyagi@users.noreply.github.com>
2022-10-09 12:18:18 +0000

    [finish]3-8

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,11 +5,14 @@ class ListsController < ApplicationController
 
   def create
     # 1.&2. データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
+    @list = List.new(list_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
-    list.save
-    # 4. トップ画面へリダイレクト
-    redirect_to list_path(list.id)
+    if @list.save
+      # 4. トップ画面へリダイレクト
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
   end
 
   def index
